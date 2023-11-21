8328c0b8e070eccbc842930140bb2c4492485de0
kvmelxv <kamelch10@outlook.com>
2023-11-20 20:37:27 -0500

    Modif Index Webdev

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 session_start();
 
 
-define('PATH_DIR', 'http://localhost/phpfiles/582-31B-MA-TP2-Location-App-/');
-//define('PATH_DIR', 'https://e2395368.webdev.cmaisonneuve.qc.ca/582-31B-MA-TP2-Location-App-/');
+//define('PATH_DIR', 'http://localhost/phpfiles/582-31B-MA-TP2-Location-App-/');
+define('PATH_DIR', 'https://e2395368.webdev.cmaisonneuve.qc.ca/582-31B-MA-TP2-Location-App-/');
 require_once('controller/Controller.php');
 require_once('library/RequirePage.php');
 require_once __DIR__.'/vendor/autoload.php';
 require_once('library/Twig.php');
 
-//$url = isset($_SERVER['PATH_INFO'])? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : '/';
-$url = isset($_GET["url"]) ? explode ('/', ltrim($_GET["url"], '/')) : '/';
+$url = isset($_SERVER['PATH_INFO'])? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : '/';
+//$url = isset($_GET["url"]) ? explode ('/', ltrim($_GET["url"], '/')) : '/';
 
 if($url == '/'){
     require_once('controller/ControllerHome.php');
